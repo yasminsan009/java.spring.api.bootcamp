f5845f54954b438258d073ba6416bbf4acf39c24
Yasmin <yasminsan19009@gmail.com>
2024-07-25 19:58:11 -0300

    commit

--- src/main/java/me/dio/api/domain/model/UserNumber.java
+++ src/main/java/me/dio/api/domain/model/UserNumber.java
@@ -1,5 +1,6 @@
 package me.dio.api.domain.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,6 +11,7 @@ public class UserNumber {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private String number;
 
     public String getNumber() {
