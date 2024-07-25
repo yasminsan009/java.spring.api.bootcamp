f8e1a74dd8834e8b7a4a13ace3ab9a5093a0c5d2
Yasmin <yasminsan19009@gmail.com>
2024-07-25 18:44:45 -0300

    third commit

--- src/main/java/me/dio/api/Controller/UserController.java
+++ src/main/java/me/dio/api/Controller/UserController.java
@@ -3,14 +3,19 @@ import java.net.URI;
 
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import me.dio.api.Service.UserService;
 import me.dio.api.domain.model.User;
 
@@ -25,7 +30,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{user}")
     public ResponseEntity<User> findById(@PathVariable Long id) throws NotFoundException{
         var user = userService.findById(id);
         return ResponseEntity.ok(user);
@@ -41,5 +46,31 @@ public class UserController {
             .toUri();
         return ResponseEntity.created(location).body(userCreated);
         
-    } 
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update a user", description = "Update the data of an existing user based on its ID")
+    @ApiResponses(value = { 
+            @ApiResponse(responseCode = "200", description = "User updated successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "422", description = "Invalid user data provided")
+    })
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        var updateUser = userService.update(id, user);
+        return ResponseEntity.ok(updateUser);
+        
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a user", description = "Delete an existing user based on its ID")
+    @ApiResponses(value = { 
+            @ApiResponse(responseCode = "204", description = "User deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })        
+    
+
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/me/dio/api/domain/model/Repository/UserRepository.java
+++ src/main/java/me/dio/api/domain/model/Repository/UserRepository.java
@@ -1,10 +1,12 @@
 package me.dio.api.domain.model.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import me.dio.api.domain.model.UserNumber;
 import me.dio.api.domain.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     boolean existsByUserNumber(UserNumber Number);
     
